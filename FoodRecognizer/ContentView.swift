e5f7cc81e2bd54b3a332d669059abdb80bb9ece7
Agustina Behotas <agustina.behotas@advancedmd.com>
2025-05-28 11:43:29 -0300

    Initial Commit

---
+++ FoodRecognizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodRecognizer
+//
+//  Created by Agustina Behotas on 28/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodRecognizer/FoodRecognizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecognizerApp.swift
+//  FoodRecognizer
+//
+//  Created by Agustina Behotas on 28/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecognizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
