6c3af5cd3b0e16e1c556770d844d4ca970b263ad
Agustina Behotas <agustina.behotas@advancedmd.com>
2025-05-28 13:17:49 -0300

    create model and viewmodel

---
+++ FoodRecognizer/Models/FoodItem.swift
@@ -0,0 +1,13 @@
+//
+//  FoodItem.swift
+//  FoodRecognizer
+//
+//  Created by Agustina Behotas on 28/05/2025.
+//
+
+import Foundation
+
+struct FoodItem {
+    let name: String
+    let confidence: Double
+}
---
+++ FoodRecognizer/ViewModels/FoodRecognizerViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  FoodRecognizerViewModel.swift
+//  FoodRecognizer
+//
+//  Created by Agustina Behotas on 28/05/2025.
+//
+
+import Foundation
+import UIKit
